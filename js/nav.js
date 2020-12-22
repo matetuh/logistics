f771f7ef69fe75184c1bd508e40d2109a2ca5a66
matetuh <matetuh@gmail.com>
2020-12-22 23:39:50 +0100

    media quaries adapt

---
+++ js/nav.js
@@ -0,0 +1,56 @@
+// Get the navbar
+var navbar = document.querySelector(".nav__box");
+var navBox = document.querySelector("#nav");
+var navLogo = document.querySelector(".nav__logo");
+var navIcon = document.querySelector(".nav__icon");
+
+
+// Get the offset height of the navBox
+var sticky = navBox.offsetHeight+60;
+
+
+// Add the sticky class to the navbar when you reach its scroll position. 
+// Remove "sticky" when you leave the scroll position
+function myFunction() {
+
+  if (window.scrollY >= sticky) {
+    navbar.classList.add("sticky");
+    navIcon.classList.add("sticky__icon");
+    navLogo.classList.remove("sticky__logo");
+
+  } else {
+    navbar.classList.remove("sticky");
+    navIcon.classList.remove("sticky__icon");
+    navLogo.classList.add("sticky__logo");
+
+  }
+}
+
+$('.nav__box a').click(function() {
+    $('.js--nav__icon').click();
+});
+
+
+window.addEventListener("scroll", myFunction);
+
+$('.js--nav__icon').click(function() {
+    var nav = $('.js--nav__box');
+    var logo = $('.nav__logo');
+    var icon = $('.js--nav__icon i');
+    var box = $('nav__box');
+
+
+
+    nav.slideToggle(200);
+
+    if(icon.hasClass('fa-times')) {
+        icon.addClass('fa-bars');
+        icon.removeClass('fa-times')
+        box.addClass('margin-down--small')
+    } else {
+        icon.addClass('fa-times');
+        icon.removeClass('fa-bars');
+        box.removeClass('margin-down--small')
+    }
+});
+
